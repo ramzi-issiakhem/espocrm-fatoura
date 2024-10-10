65dd9982e230c3759982f7666cf359eb6e2c2167
Issiakhem Mohamed Ramzi <issiakhem.mohamedramzi@gmail.com>
2024-10-10 11:31:18 +0100

    Added default assignement

--- custom/Espo/Custom/Hooks/Lead/LeadBeforeSave.php
+++ custom/Espo/Custom/Hooks/Lead/LeadBeforeSave.php
@@ -6,8 +6,6 @@ use Espo\Core\Exceptions\ConflictSilent;
 use Espo\Core\Field\DateTime;
 use Espo\Core\Hook\Hook\BeforeSave;
 use Espo\Core\Record\CreateParams;
-use Espo\Core\Select\SearchParams;
-use Espo\Core\Select\Where\Item;
 use Espo\Modules\Crm\Entities\Call;
 use Espo\Modules\Crm\Entities\Lead;
 use Espo\Modules\Crm\Entities\Reminder;
@@ -17,7 +15,6 @@ use Espo\ORM\Repository\Option\SaveOptions;
 use Espo\Core\Record\ServiceContainer;
 use \Espo\Core\Record\Service;
 use Espo\ORM\Repository\RDBRepository;
-use Espo\ORM\Repository\Repository;
 
 class LeadBeforeSave implements BeforeSave
 {
@@ -42,8 +39,15 @@ class LeadBeforeSave implements BeforeSave
      */
     public function beforeSave(Entity $entity, SaveOptions $options): void
     {
-        $stage  = $entity->get('status');
-        $leadCAllStatus = $entity->get("cCallStatus");
+        $stage = $entity->get('status');
+        $leadCallStatus = $entity->get("cCallStatus");
+
+        // Set the Assigned User to the Created By User if not defined
+        if ($entity->isNew()) {
+            if ($entity->get('assignedUserId') == null) {
+                $entity->set('assignedUserId', $entity->get('createdById') );
+            }
+        }
 
         // Check if the lead is going to Won STage and if the quote number is defined
         if ($entity->isAttributeChanged('status')) {
@@ -53,51 +57,47 @@ class LeadBeforeSave implements BeforeSave
         }
 
         if ($entity->isAttributeChanged('cCallStatus')) {
-            if (in_array($leadCAllStatus, ["Call Back","Did not pick up"])) {
-                    $name = 'Call - ' . $entity->get('name');
-                    $callEntity = $this->callRepository->where(['name' => $name])->findOne();
+            if (in_array($leadCallStatus, ["Call Back", "Did not pick up"])) {
+                $name = 'Call - ' . $entity->get('name');
+                $callEntity = $this->callRepository->where(['name' => $name])->findOne();
 
 
-                    if ($callEntity == null) {
-                        $callEntity = $this->entityManager->getEntity(Call::ENTITY_TYPE);
-                    }
-
-                    // Create a Delay of 3 Days
-                    $dateTime = DateTime::createNow();
-                    $dateTime = $dateTime->withTime(9, 0, 0)->addDays(3)->toString();
-
-                    // Create a Call
-                    $callEntity->set('dateStart', $dateTime);
-                    $callEntity->set('name', $name);
-                    $callEntity->set('description', 'Call Back');
-                    $callEntity->set('assignedUserId', $entity->get('assignedUserId'));
-                    $callEntity->set('teamsIds', $entity->get('teamsIds'));
-                    $this->entityManager->saveEntity($callEntity);
-
-                    // Create a Reminder and link it to the call
-                    $data = (object) [
-                        'type' => Reminder::TYPE_POPUP,
-                        'seconds' => 0,
-                        'entityType' => Call::ENTITY_TYPE,
-                        'entityId' => $callEntity->getId(),
-                        'userId' => $entity->get('assignedUserId'),
-                        'reminderTime' => $dateTime,
-                    ];
-                    $this->reminderRecord->create($data,CreateParams::create());
-
-                    // Link the Call to the Lead
-                    $this->leadRecord->link($entity->getId(), 'calls', $callEntity->getId());
+                if ($callEntity == null) {
+                    $callEntity = $this->entityManager->getEntity(Call::ENTITY_TYPE);
                 }
-        }
-        else if ($leadCAllStatus == "Wrong Info" || $leadCAllStatus == "Not Interested") {
+
+                // Create a Delay of 3 Days
+                $dateTime = DateTime::createNow();
+                $dateTime = $dateTime->withTime(9, 0, 0)->addDays(3)->toString();
+
+                // Create a Call
+                $callEntity->set('dateStart', $dateTime);
+                $callEntity->set('name', $name);
+                $callEntity->set('description', 'Call Back');
+                $callEntity->set('assignedUserId', $entity->get('assignedUserId'));
+                $callEntity->set('teamsIds', $entity->get('teamsIds'));
+                $this->entityManager->saveEntity($callEntity);
+
+                // Create a Reminder and link it to the call
+                $data = (object)[
+                    'type' => Reminder::TYPE_POPUP,
+                    'seconds' => 0,
+                    'entityType' => Call::ENTITY_TYPE,
+                    'entityId' => $callEntity->getId(),
+                    'userId' => $entity->get('assignedUserId'),
+                ];
+                $this->reminderRecord->create($data, CreateParams::create());
+
+                // Link the Call to the Lead
+                $this->leadRecord->link($entity->getId(), 'calls', $callEntity->getId());
+            }
+            else if ($leadCallStatus == "Wrong Info" || $leadCallStatus == "Not Interested") {
                 $entity->set('status', "Lost");
             }
-        else if ($leadCAllStatus == "Interested") {
-                $entity->set('status', "Qualified");
-            }
 
 
         }
+    }
 
 
 }
